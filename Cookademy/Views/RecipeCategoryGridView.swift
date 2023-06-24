6f492da8655b53bc91d1ef0c76350b49b9b7ef77
Rich Polanco <rich.polanco@automattic.com>
2023-06-23 19:29:42 -0600

    Refactor grid and extract Category

--- Cookademy/Views/RecipeCategoryGridView.swift
+++ Cookademy/Views/RecipeCategoryGridView.swift
@@ -11,14 +11,31 @@ struct RecipeCategoryGridView: View {
     private var recipeData = RecipeData()
 
     var body: some View {
+        let columns = [GridItem(), GridItem()]
         NavigationView {
-            LazyVGrid(columns: [GridItem()], content: {
-                ForEach(MainInformation.Category.allCases, id: \.self) { category in
-                    Text(category.rawValue)
-                        .font(.title)
-                }
-            })
-            .navigationTitle("Categories")
+            ScrollView {
+                LazyVGrid(columns: columns, content: {
+                    ForEach(MainInformation.Category.allCases, id: \.self) { category in
+                        CategoryView(category: category)
+                    }
+                })
+                .navigationTitle("Categories")
+            }
+        }
+    }
+}
+
+struct CategoryView: View {
+    let category: MainInformation.Category
+
+    var body: some View {
+        ZStack {
+            Image(category.rawValue)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .opacity(0.35)
+            Text(category.rawValue)
+                .font(.title)
         }
     }
 }
