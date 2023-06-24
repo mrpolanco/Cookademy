ba34ec975a34eec43ade12017a26344754ab1194
Rich Polanco <rich.polanco@automattic.com>
2023-06-23 18:10:09 -0600

    Adds Grid View

---
+++ Cookademy/Views/RecipeCategoryGridView.swift
@@ -0,0 +1,29 @@
+//
+//  RecipeCategoryGridView.swift
+//  Cookcademy
+//
+//  Created by Rich Polanco on 6/23/23.
+//
+
+import SwiftUI
+
+struct RecipeCategoryGridView: View {
+    private var recipeData = RecipeData()
+
+    var body: some View {
+        NavigationView {
+            LazyVGrid(columns: [GridItem()], content: {
+                ForEach(MainInformation.Category.allCases, id: \.self) { category in
+                    Text(category.rawValue)
+                }
+            })
+            .navigationTitle("Categories")
+        }
+    }
+}
+
+struct RecipeCategoryGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCategoryGridView()
+    }
+}
